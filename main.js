4aa41388451dc5d54533533da533fbee8068d6e3
Strahinja Rolović <126403008+strahinjarol@users.noreply.github.com>
2023-04-29 01:39:28 +0200

    Initial upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// hanburger-menu
+const navMenu = document.querySelector(".nav-menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const xMenu = document.querySelector(".xmenu");
+
+hamburgerMenu.addEventListener("click", show);
+xMenu.addEventListener("click", hide);
+
+function show() {
+  navMenu.style.display = "flex";
+  navMenu.style.top = "0";
+}
+
+function hide() {
+  navMenu.style.top = "-100%";
+}
+
+// sticky navbar
+
+const navBar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  this.scrollY > 20
+    ? navBar.classList.add("sticky")
+    : navBar.classList.remove("sticky");
+};
